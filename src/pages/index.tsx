f0993a337f807ed28d431cabb5a2294ab52ad0c2
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2024-11-11 22:41:42 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,35 +86,37 @@ interface NavItemProps {
   ) => {
     const { name, value, type, files } = e.target as HTMLInputElement;
 
-    setFormData((prevData) => {
-  const updatedFormData = { ...prevData };
+   
 
-  if (sectionIndex !== undefined) {
-    // Handle array fields (experience, education, skills)
-    if (field === 'experience') {
-      const updatedExperience = [...prevData.experience];
-      updatedExperience[sectionIndex] = {
-        ...updatedExperience[sectionIndex],
-        [name]: value,
-      };
-      updatedFormData.experience = updatedExperience;
-    } else if (field === 'education') {
-      const updatedEducation = [...prevData.education];
-      updatedEducation[sectionIndex] = {
-        ...updatedEducation[sectionIndex],
-        [name]: value,
-      };
-      updatedFormData.education = updatedEducation;
-    } else if (field === 'skills') {
-      const updatedSkills = [...prevData.skills];
-      updatedSkills[sectionIndex] = value;
-      updatedFormData.skills = updatedSkills;
-    }
-  } else if (type === 'file') {
-    updatedFormData.image = files[0];
-  } else {
-    updatedFormData[name] = value;
-  }
+    setFormData((prevData) => {
+      const updatedFormData = { ...prevData };
+    
+      if (sectionIndex !== undefined) {
+        // Handle array fields (experience, education, skills)
+        if (field === 'experience') {
+          const updatedExperience = [...prevData.experience];
+          updatedExperience[sectionIndex] = {
+            ...updatedExperience[sectionIndex],
+            [name]: value,
+          };
+          updatedFormData.experience = updatedExperience;
+        } else if (field === 'education') {
+          const updatedEducation = [...prevData.education];
+          updatedEducation[sectionIndex] = {
+            ...updatedEducation[sectionIndex],
+            [name]: value,
+          };
+          updatedFormData.education = updatedEducation;
+        } else if (field === 'skills') {
+          const updatedSkills = [...prevData.skills];
+          updatedSkills[sectionIndex] = value;
+          updatedFormData.skills = updatedSkills;
+        }
+      } else if (e.target.files && e.target.files.length>0) {
+        updatedFormData.image = e.target.files[0];
+      } else {
+        updatedFormData[name] = value;
+      }
 
       const isModified = JSON.stringify(updatedFormData) !== JSON.stringify(initialFormData);
       setModified(isModified);
